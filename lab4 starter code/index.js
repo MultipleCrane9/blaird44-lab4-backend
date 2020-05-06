f15240dcc809b3853af2008fe9a7e0d980b286fa
MultipleCrane9 <bjjlaird@hotmail.com>
2020-05-06 12:14:22 -0700

    add get/post with arguments

--- lab4 starter code/index.js
+++ lab4 starter code/index.js
@@ -35,14 +35,14 @@ app.get("/params", (request, response) => {
     response.send({
     //req.query is a reference to arguments in the POST body
     message: "Hello, " + request.query.name + "! You sent a GET Request"
-    })
+})
     } else {
     response.status(400)
-    response.send({
-    message: "Missing required information"
-    })
+        response.send({
+        message: "Missing required information"
+        })
     }
-   })
+})
    
    app.post("/params", (request, response) => {
     if (request.body.name) {
